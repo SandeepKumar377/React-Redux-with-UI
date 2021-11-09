359a998e8c33a4a449f4a459c879c38a949cfd70
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-09 14:22:02 +0530

    Done cakeStore

--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,17 +1,15 @@
 import { BUY_CAKE } from "./cakeTypes"
 
-const initialState={
-    numOfCakes:10
+const initialState = {
+    numOfCakes: 10
 }
-const cakeReducer=(state = initialState, action)=>{
-    switch(action.type){
-        case BUY_CAKE:
-            return{
-                ...state,
-                numOfCakes: state.numOfCakes-1
-            }
-        default:
-            return state
+const cakeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
     }
 }
 export default cakeReducer
